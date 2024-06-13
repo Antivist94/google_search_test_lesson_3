3b248c963ebcf71b6a29e94dd640ff8bbd94bcff
Eugene Evdokimenko <evdokimenko@tochka.com>
2024-06-13 09:53:31 +0400

    update confest and tests

---
+++ conftest.py
@@ -0,0 +1,11 @@
+from selene import browser
+import pytest
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_manager():
+    browser.config.window_width = '1920'
+    browser.config.window_height = '1080'
+    browser.config.base_url = 'https://google.com'
+    yield
+    browser.quit()
